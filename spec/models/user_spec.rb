500399468740f4a5691eab7c595500715e74988d
Tu Ngo <tungo8810@gmail.com>
2017-04-21 12:17:05 -0700

    Add User rspec instance methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,20 @@ RSpec.describe User, type: :model do
   subject(:user) { FactoryGirl.build(:user) }
 
   describe "validations" do
-    # it "has unique username and session token"
-    describe "has not null username, password_digest, and session token" do
+    describe "has username, password_digest, and session token" do
       it { should validate_presence_of(:username) }
       it { should validate_presence_of(:password_digest) }
       it { should validate_presence_of(:session_token) }
     end
-    #it "validates password"
+    describe "has unique username and session token" do
+      it { should validate_uniqueness_of(:username) }
+      it { should validate_uniqueness_of(:session_token) }
+    end
+
+    describe "password must be 6 characters or longer" do
+      #password length
+      it { should validate_length_of(:password).is_at_least(6) }
+    end
   end
 
   describe "associations" do
@@ -19,19 +26,48 @@ RSpec.describe User, type: :model do
 
   describe "instance methods" do
     describe "#is_password?" do
-
-    end
-
-    describe "#reset_session_token" do
-
+      it "can confirm the password" do
+        expect(user.is_password("password")).to be_true
+      end
+      it "can reject the incorrect password" do
+        expect(user.is_password("not_password")).to be_false
+      end
     end
 
     describe "#ensure_session_token" do
+      it "generate the session" do
+        expect(user.session_token).not_to be_nil
+      end
+    end
 
+    describe "#reset_session_token!" do
+      it "reassign session token" do
+        curr_token = user.session_token
+        user.reset_session_token!
+
+        expect(user.session_token).not_to be(curr_token)
+      end
     end
 
     describe "#password=" do
+      it "assigns password digest" do
+        expect(user.password_digest).not_to be_nil
+      end
 
+      it "replaces old password" do
+        user.password = 'new_password'
+        expect(user.password).to be('new_password')
+
+        expect(BCrypt::Password.new(user.password_digest).is_password?('new_password')).to be_true
+      end
+
+      it "password not store in database" do
+        user.save
+        m_user = User.find_by(username: user.username)
+
+        expect(m_user.password).to be_nil
+        expect(user.password).not_to be_nil
+      end
     end
   end
 
